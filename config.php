1323c50d6350dda129c3308c0aab3ebab65a8384
xiangang.wei <xiangang.wei@feixun.com.cn>
2016-09-01 14:02:53 +0800

    * change the MySQL info

--- config.php
+++ config.php
@@ -6,8 +6,15 @@
  ******************************************************/
 
 //We log to the DataBase
-mysql_connect('host', 'username', 'password');
-mysql_select_db('database_name');
+define("LOCALHOST", "172.0.0.1");
+define("DBUSER", "forum");
+define("DBPASSWORD", "forum123");
+define("DBNAME", "forum");
+
+//mysql_connect('host', 'username', 'password');
+//mysql_select_db('database_name');
+@ $db = new mysqli(LOCALHOST, DBUSER, DBPASSWORD, DBNAME);
+
 //Username of the Administrator
 $admin = 'admin';
 
